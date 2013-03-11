7e66959813b65272ade147627804483d1f0b97ff
Simon Lundmark <simon.lundmark@gmail.com>
2013-03-11 19:16:09 +0100

    Add separate project for web site

---
+++ mmorps-web/project.clj
@@ -0,0 +1,9 @@
+(defproject mmorps-web "0.1.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [compojure "1.1.5"]]
+  :plugins [[lein-ring "0.8.2"]]
+  :ring {:handler mmorps-web.handler/app}
+  :profiles {:dev {:dependencies [[ring-mock "0.1.3"]
+                                  [midje "1.5.0"]]
+                   :plugins [[lein-midje "3.0.0"]]}})
+
