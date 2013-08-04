dfaa2bf98e4b69f379e65999dff20ba947159897
Simon Lundmark <simon.lundmark@gmail.com>
2013-08-04 16:51:42 +0200

    Upgrade clojure and midje to latest versions

--- mmorps-web/project.clj
+++ mmorps-web/project.clj
@@ -1,10 +1,10 @@
 (defproject mmorps-web "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.0"]
                  [compojure "1.1.5"]
                  [ring/ring-json "0.2.0"]]
   :plugins [[lein-ring "0.8.2"]]
   :ring {:handler mmorps-web.handler/app}
   :profiles {:dev {:dependencies [[ring-mock "0.1.3"]
-                                  [midje "1.5.0"]]
-                   :plugins [[lein-midje "3.0.0"]]}})
+                                  [midje "1.5.1"]]
+                   :plugins [[lein-midje "3.1.0"]]}})
 
--- mmorps/project.clj
+++ mmorps/project.clj
@@ -1,5 +1,5 @@
 (defproject mmorps "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.4.0"]]
-  :profiles {:dev {:dependencies [[midje "1.5.0"]]
-                   :plugins [[lein-midje "3.0.0"]]}})
+  :dependencies [[org.clojure/clojure "1.5.0"]]
+  :profiles {:dev {:dependencies [[midje "1.5.1"]]
+                   :plugins [[lein-midje "3.1.0"]]}})
 
